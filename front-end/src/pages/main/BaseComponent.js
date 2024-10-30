228abee85e2bee59234fecf12293bf539583ca02
RobbieT17 <robbiewthurston@gmail.com>
2024-10-30 11:04:22 -0400

    Implemented base component (ALL COMPONENTS MUST EXTEND FROM THIS CLASS)

--- front-end/src/pages/main/BaseComponent.js
+++ front-end/src/pages/main/BaseComponent.js
@@ -1,17 +1,57 @@
 export class BaseComponent {
-  constructor() {
+  constructor(id) {
+    this.body = document.createElement('div')
+    this.body.classList.add('view')
+    this.body.id = id
+
+    const views = document.getElementById('views')
+
+    views.appendChild(this.body)
+    this.#initialize()
     //   this.cssLoaded = false;
+  }
+
+// Abstract Methods
+  /**
+   * This is an abstract method that must be implemented by child classes.
+   * It must return an HTMLElement object.
+   * @abstract
+   */
+  _buildHTML() {
+    throw new Error("_buildHTML method not implemented");
   }
 
   /**
    * This is an abstract method that must be implemented by child classes.
    * It must return an HTMLElement object.
    * @abstract
-   * @returns {HTMLElement}
    */
-  render() {
+  _addCustomEventListeners() {
+    throw new Error("_addCustomEventListeners method not implemented");
+  }
+
+  /**
+   * This is an abstract method that must be implemented by child classes.
+   * It must return an HTMLElement object.
+   * @abstract
+   */
+  _render(data) {
     throw new Error("render method not implemented");
   }
+
+// Methods
+  _changeDisplay(view) {
+    document.getElementById(this.body.id).style.display = view
+  }
+
+  #initialize() {
+    this._buildHTML()
+    this._addCustomEventListeners()
+    this._changeDisplay('none')
+  }
+
+
+
   // ***********************************************
   // **TODO**: Implement CSS (not a priority for M3)
   // ***********************************************
@@ -27,12 +67,12 @@ export class BaseComponent {
   //   this.cssLoaded = true;
   // }
 
-  dispatchCustomEvent(eventName, detail = {}) {
-    const event = new CustomEvent(eventName, { detail });
-    this.parent.dispatchEvent(event);
-  }
+  // dispatchCustomEvent(eventName, detail = {}) {
+  //   const event = new CustomEvent(eventName, { detail });
+  //   this.parent.dispatchEvent(event);
+  // }
 
-  listenToEvent(eventName, callback) {
-    this.parent.addEventListener(eventName, callback);
-  }
+  // listenToEvent(eventName, callback) {
+  //   this.parent.addEventListener(eventName, callback);
+  // }
 }
