20c0768404ec2f6e3bbd5bfc23671829c2617e7e
Anugrah George <anugrahgeorg@umass.edu>
2024-11-01 16:42:54 -0400

    Added previous and forward month navigation
    Edited CalendarComponent.js to add previous and forward navigation

--- front-end/src/pages/calendar/CalendarComponent.js
+++ front-end/src/pages/calendar/CalendarComponent.js
@@ -6,6 +6,20 @@ export class CalendarComponent extends BaseComponent {
   constructor() {
     super("calendarPage", "./pages/calendar/stylesCalendar.css");
     this.dateData = {};
+    this._loadFontAwesome();
+    this.date = new Date(); // Define `this.date` as a class property
+  }
+
+  /**
+   * Loads the Font Awesome stylesheet for icons.
+   */
+  _loadFontAwesome() {
+    if (!document.querySelector("link[href*='font-awesome']")) { // Check if not already added
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css";
+      document.head.appendChild(link);
+    }
   }
 
   /**
@@ -82,28 +96,28 @@ export class CalendarComponent extends BaseComponent {
 
   // Renders the calendar
   #renderCalendar() {
-    const date = new Date();
-    date.setDate(1);
+    //const date = new Date();
+    this.date.setDate(1);
 
     const monthDays = document.querySelector(".days");
 
     const lastDay = new Date(
-      date.getFullYear(),
-      date.getMonth() + 1,
+      this.date.getFullYear(),
+      this.date.getMonth() + 1,
       0
     ).getDate();
 
     const prevLastDay = new Date(
-      date.getFullYear(),
-      date.getMonth(),
+      this.date.getFullYear(),
+      this.date.getMonth(),
       0
     ).getDate();
 
-    const firstDayIndex = date.getDay();
+    const firstDayIndex = this.date.getDay();
 
     const lastDayIndex = new Date(
-      date.getFullYear(),
-      date.getMonth() + 1,
+      this.date.getFullYear(),
+      this.date.getMonth() + 1,
       0
     ).getDay();
 
@@ -124,7 +138,7 @@ export class CalendarComponent extends BaseComponent {
       "December",
     ];
 
-    document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+    document.querySelector(".date h1").innerHTML = months[this.date.getMonth()];
     document.querySelector(".date p").innerHTML = new Date().toDateString();
 
     let days = "";
@@ -136,7 +150,7 @@ export class CalendarComponent extends BaseComponent {
     for (let i = 1; i <= lastDay; i++) {
       if (
         i === new Date().getDate() &&
-        date.getMonth() === new Date().getMonth()
+        this.date.getMonth() === new Date().getMonth()
       ) {
         days += `<div class="today">${i}</div>`;
       } else {
@@ -146,23 +160,24 @@ export class CalendarComponent extends BaseComponent {
 
     for (let j = 1; j <= nextDays; j++) {
       days += `<div class="next-date">${j}</div>`;
-      monthDays.innerHTML = days;
+      
     }
+    monthDays.innerHTML = days;
   }
-  monthDays.innerHTML = days;
 
   // Adds event listeners to the prev and next buttons as well as
   // the feature buttons
   _addEventListeners() {
     const hub = EventHub.getInstance();
     hub.subscribe(Events.LoadMainPage, (data) => this._render(data));
+
     document.querySelector(".prev").addEventListener("click", () => {
-      date.setMonth(date.getMonth() - 1);
+      this.date.setMonth(this.date.getMonth() - 1);
       this.#renderCalendar();
     });
 
     document.querySelector(".next").addEventListener("click", () => {
-      date.setMonth(date.getMonth() + 1);
+      this.date.setMonth(this.date.getMonth() + 1);
       this.#renderCalendar();
     });
 
