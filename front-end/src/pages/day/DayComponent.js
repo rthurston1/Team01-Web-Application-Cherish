619b923a3af9eeb9368408f78625e1a4878ee1ce
RobbieT17 <robbiewthurston@gmail.com>
2024-11-10 12:25:57 -0500

    Added function to go back to main page

--- front-end/src/pages/day/DayComponent.js
+++ front-end/src/pages/day/DayComponent.js
@@ -1,7 +1,6 @@
 import { EventHub } from '../../eventhub/EventHub.js'
 import { Events } from '../../eventhub/Events.js'
 import { BaseComponent } from '../main/BaseComponent.js'
-import { JournalComponent } from './journal/JournalComponent.js'
 
 export class DayComponent extends BaseComponent {
     constructor() {
@@ -11,15 +10,18 @@ export class DayComponent extends BaseComponent {
 
 // Methods
 
+    #goToMainPage() {
+        EventHub.getInstance().publish(Events.LoadMainPage, this.dateData)
+    }
+
     // Calls an event to load journal page
     #goToJournalPage() {
-        const hub = EventHub.getInstance()
-        hub.publish(Events.LoadJournalPage, this.dateData)
+        EventHub.getInstance().publish(Events.LoadJournalPage, this.dateData)
     }
 
     // Calls an event to load check-in page
     #goToCheckInPage() {
-        // TODO: Implement this method
+        EventHub.getInstance().publish(Events.LoadCheckInPage, this.dateData)
     }
 
     // Appends new emotion entry to Emotion Log
@@ -35,42 +37,40 @@ export class DayComponent extends BaseComponent {
 // Inherited Methods from BaseComponent
     _buildHTML() {
         return `
-            <h1 class="body-element" id="date"></h1>
+            <div class="day-container">
+                <h1 id="dayDate"></h1>
 
-            <div class="body-element" id="content">
-                <div class="scroll-container" id="emotionLog"></div>
+                <div class="day-body-element" id="dayContent">
+                    <div class="day-log-container" id="dayEmotionLog"></div>
 
-                <div class="journal-container" id="journalLog">
-                    <div class="button-container" id="buttons">
-                        <button id="toJournalPage">Journal</button>
-                        <button id="toCheckInPage">Check-In</button>
+                    <div class="day-log-container" id="dayJournalLog">
+                        <textarea id="dayJournalEntry" placeholder="No journal entry" readonly></textarea>
+
+                        <div class="day-button-container" id="dayButtons">
+                            <button id="dayToMain">Main Page</button>
+                            <button id="dayToJournal">Journal</button>
+                            <button id="dayToCheckIn">Check-In</button>
+                        </div>
                     </div>
-
-                    <textarea id="journalEntry" placeholder="No journal entry" readonly></textarea>
                 </div>
-
             </div>
         `
     }
 
     _addEventListeners() {
         const hub = EventHub.getInstance()
-        hub.subscribe(Events.LoadDayPage, data => this._render(data))
+        hub.subscribe(Events.LoadDayPage, data => this.loadPage(data))
 
-        document.getElementById('toJournalPage').addEventListener('click', () => this.#goToJournalPage())
-        document.getElementById('toCheckInPage').addEventListener('click', () => this.#goToCheckInPage())
+        document.getElementById('dayToMain').addEventListener('click', () => this.#goToMainPage())
+        document.getElementById('dayToJournal').addEventListener('click', () => this.#goToJournalPage())
+        document.getElementById('dayToCheckIn').addEventListener('click', () => this.#goToCheckInPage())
     }
 
    // Changes view to Day Page
     _render(data) {
-        document.querySelectorAll('.view').forEach(body => body.style.display = 'none')
-
         this.dateData = data
-        document.getElementById('date').textContent = this.dateData.format 
-        document.getElementById('journalEntry').textContent = this.dateData.journal_entry
-
-        // Displays View
-        this._changeDisplay('flex')
+        document.getElementById('dayDate').textContent = this.dateData.format 
+        document.getElementById('dayJournalEntry').textContent = this.dateData.journal_entry
     }
 
 }
