0c1a3ddb1245fc406d04f97541d751f83da2b402
RobbieT17 <robbiewthurston@gmail.com>
2024-11-10 14:13:08 -0500

    Implemented add/remove emotions

--- front-end/src/pages/day/DayComponent.js
+++ front-end/src/pages/day/DayComponent.js
@@ -24,29 +24,54 @@ export class DayComponent extends BaseComponent {
         EventHub.getInstance().publish(Events.LoadCheckInPage, this.dateData)
     }
 
+    #addJournalEntry(journal) {
+        this.dateData['journal_entry'] = journal
+        EventHub.getInstance().publish(Events.UpdateDatabase, this.dateData)
+    }
+
     // Appends new emotion entry to Emotion Log
     #addEmotionEntry(emotion_entry) {
-        // TODO: Implement this method
+        if (!this.dateData['emotions']) this.dateData['emotions'] = []
+        this.dateData.emotions.push(emotion_entry)
+        this.#calculateRating()
     }
 
     // Removes the specified emotion element from the Emotion Log
     #removeEmotionEntry(emotion_entry) {
-       // TODO: Implement this method
+       this.dateDate.emotions = this.dateData.emotions.filter(e => e !== emotion_entry)
+       this.#calculateRating()
+    }
+
+    // Calculates Daily Ranking based on emotions logged also saves any changed to database
+    #calculateRating() {
+
+        EventHub.getInstance().publish(Events.UpdateDatabase, this.dateData)
     }
 
 // Inherited Methods from BaseComponent
     _buildHTML() {
         return `
             <div class="day-container">
-                <h1 id="dayDate"></h1>
-
+                <div class="day-head-element">
+                    <h1>Day Page<h1>
+                    <h2 id="dayDate">Hello</h2>
+                </div>
+                
                 <div class="day-body-element" id="dayContent">
-                    <div class="day-log-container" id="dayEmotionLog"></div>
+                    <div class="day-emotion-container">
+                        <div class="day-log-entry">
+                            <label>Hello1</label>
+                        </div>
+                        <div class="day-log-entry">
+                            <label>Hello2</label>
+                        </div>
+                        
+                    </div>
 
-                    <div class="day-log-container" id="dayJournalLog">
+                    <div class="day-journal-container">
                         <textarea id="dayJournalEntry" placeholder="No journal entry" readonly></textarea>
 
-                        <div class="day-button-container" id="dayButtons">
+                        <div id="dayButtons">
                             <button id="dayToMain">Main Page</button>
                             <button id="dayToJournal">Journal</button>
                             <button id="dayToCheckIn">Check-In</button>
@@ -60,6 +85,8 @@ export class DayComponent extends BaseComponent {
     _addEventListeners() {
         const hub = EventHub.getInstance()
         hub.subscribe(Events.LoadDayPage, data => this.loadPage(data))
+        hub.subscribe(Events.SummarySubmitted, journal => this.#addJournalEntry(journal))
+        hub.subscribe(Events.CheckInSubmitted, emotion => this.#addEmotionEntry(emotion))
 
         document.getElementById('dayToMain').addEventListener('click', () => this.#goToMainPage())
         document.getElementById('dayToJournal').addEventListener('click', () => this.#goToJournalPage())
@@ -71,6 +98,8 @@ export class DayComponent extends BaseComponent {
         this.dateData = data
         document.getElementById('dayDate').textContent = this.dateData.format 
         document.getElementById('dayJournalEntry').textContent = this.dateData.journal_entry
+
+        // Added Emotions to Log
     }
 
 }
