c6643d641163d7f099a81f4b20603d15f78bb803
RobbieT17 <robbiewthurston@gmail.com>
2024-11-10 20:23:25 -0500

    removed unnecessary elements from date object

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,9 +1,8 @@
+import { CalendarComponent } from "./pages/calendar/CalendarComponent.js";
 import { DayComponent } from "./pages/day/DayComponent.js";
 import { JournalComponent } from "./pages/journal/JournalComponent.js";
 import { EventHub } from "./eventhub/EventHub.js";
 import { Events } from "./eventhub/Events.js";
-
-import { CalendarComponent } from "./pages/calendar/CalendarComponent.js";
 
 new CalendarComponent();
 new DayComponent();
@@ -14,11 +13,7 @@ const today = new Date();
 const dateArr = [today.getMonth() + 1, today.getDate(), today.getFullYear()];
 
 const date = {
-  month: dateArr[0], // ex: 10 (October)
-  day: dateArr[1], // ex: 29
-  year: dateArr[2], // ex: 2024
-  format: dateArr.join("/"), // Displays in header ex: 10/29/2024
-  id: dateArr.join("-"), // ID to pass as key to localStorage ex: 10-29-2024
+  date_id: dateArr.join("-"), // ID to pass as key to localStorage ex: 10-29-2024
 };
 
 EventHub.getInstance().publish(Events.LoadMainPage, date);
