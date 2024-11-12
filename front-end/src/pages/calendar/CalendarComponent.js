07a6aa4b7311ab74fff382f8a5a0cb378b77b3a0
rthurston1 <rthurston@umass.edu>
2024-11-11 23:28:44 -0500

    removed duplicate/unused code

--- front-end/src/pages/calendar/CalendarComponent.js
+++ front-end/src/pages/calendar/CalendarComponent.js
@@ -38,34 +38,7 @@ export class CalendarComponent extends BaseComponent {
       document.head.appendChild(link);
     }
   }
-  /**
-   * Navigates to the specified page by publishing an event to the EventHub.
-   * Depending on the page parameter, it publishes different load page events.
-   * Work in progress: dateData
-   *
-   * @param {string} page - The name of the page to navigate to.
-   *                        Possible values are "check-in", "journal", "stats", "summary", or any other string for the main page.
-   */
-  #goToPage(page) {
-    const hub = EventHub.getInstance();
-    switch (page) {
-      case "check-in":
-        hub.publish(Events.LoadCheckInPage, this.dateData);
-        break;
-      case "journal":
-        hub.publish(Events.LoadJournalPage, this.dateData);
-        break;
-      case "stats":
-        hub.publish(Events.LoadStatsPage, this.dateData);
-        break;
-      case "summary":
-        hub.publish(Events.LoadSummaryPage, this.dateData);
-        break;
-      default:
-        hub.publish(Events.LoadMainPage, this.dateData);
-    }
-  }
-
+  
   // Builds the HTML of the Calendar Page
   _buildHTML() {
     return `<div class="container">
@@ -107,7 +80,7 @@ export class CalendarComponent extends BaseComponent {
       const t = e.target;
       if (t.classList.contains("day")) {
         const date = t.dataset.date;
-        hub.publish(Events.LoadDayPage, date);
+        hub.publish(Events.LoadDayPage, this.dateData);
       }
     });
 
@@ -121,21 +94,9 @@ export class CalendarComponent extends BaseComponent {
       this._render();
     });
 
-    document
-      .getElementById("main_toJournalPage")
-      .addEventListener("click", () => this.#goToPage("journal"));
-    document
-      .getElementById("main_toCheckInPage")
-      .addEventListener("click", () => this.#goToPage("check-in"));
-    document
-      .getElementById("main_toStatsPage")
-      .addEventListener("click", () => this.#goToPage("stats"));
-    document
-      .getElementById("main_toSummaryPage")
-      .addEventListener("click", () => this.#goToPage("summary"));
   }
 
-  _render(data = null) {
+  _render(data) {
     // Month offset constants for previous, current, and next month
     const PREV = 0,
       CURR = 1,
