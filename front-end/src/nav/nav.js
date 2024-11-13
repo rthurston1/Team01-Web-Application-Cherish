274961cbf07f717952c3f552992aa9144d140e94
rthurston1 <rthurston@umass.edu>
2024-11-12 21:14:33 -0500

    changed name of variable imported

--- front-end/src/nav/nav.js
+++ front-end/src/nav/nav.js
@@ -1,6 +1,6 @@
 import { EventHub } from "../eventhub/EventHub.js";
 import { Events } from "../eventhub/Events.js";
-import { date } from "../main.js";
+import { currentDate as date} from "../main.js";
 
 /**
  * Navigates to the specified page.
