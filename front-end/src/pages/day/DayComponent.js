6a962fa8bd62b08e076cb735ec5054eceb6d7967
rthurston1 <rthurston@umass.edu>
2024-11-12 21:17:51 -0500

    Removed buttons and now aligns with new BaseComponent changes.

--- front-end/src/pages/day/DayComponent.js
+++ front-end/src/pages/day/DayComponent.js
@@ -1,4 +1,3 @@
-import { EventHub } from "../../eventhub/EventHub.js";
 import { Events } from "../../eventhub/Events.js";
 import { BaseComponent } from "../../BaseComponent.js";
 import { MONTHS } from "../calendar/CalendarComponent.js";
@@ -17,44 +16,18 @@ export class DayComponent extends BaseComponent {
   constructor() {
     super("dayPage", "./pages/day/stylesDay.css");
     this.dateData = {};
+
+    // Elements
+    this.titleDate = document.getElementById("dayDate");
+    this.journalEntry =  document.getElementById("dayJournalEntry");
   }
 
-  // Methods
-
-  #goToMainPage() {
-    EventHub.getInstance().publish(Events.LoadMainPage, this.dateData);
-  }
-
-  // Calls an event to load journal page
-  #goToJournalPage() {
-    EventHub.getInstance().publish(Events.LoadJournalPage, this.dateData);
-  }
-
-  // Calls an event to load check-in page
-  #goToCheckInPage() {
-    EventHub.getInstance().publish(Events.LoadCheckInPage, this.dateData);
-  }
-
-  #addJournalEntry(journal) {
-    this.dateData["journal_entry"] = journal;
-    EventHub.getInstance().publish(Events.UpdateDatabase, this.dateData);
-  }
-
-  // Appends new emotion entry to Emotion Log
-  #addEmotionEntry(emotion_entry) {
-    if (!this.dateData["emotions"]) this.dateData["emotions"] = [];
-    this.dateData.emotions.push(emotion_entry);
-    this.#calculateRating();
-    alert("emotion added!");
-  }
-
+// Methods
   // Removes the specified emotion element from the Emotion Log
   #removeEmotionEntry(emotion_entry) {
     document.getElementById("dayEmotionLog").innerHTML = "";
 
-    const filteredArr = this.dateData.emotions.filter(
-      (e) => e !== emotion_entry
-    );
+    const filteredArr = this.dateData.emotions.filter((e) => e !== emotion_entry);
     this.dateData.emotions = filteredArr;
 
     this.#calculateRating();
@@ -64,7 +37,7 @@ export class DayComponent extends BaseComponent {
 
   // Calculates Daily Ranking based on emotions logged also saves any changed to database
   #calculateRating() {
-    EventHub.getInstance().publish(Events.UpdateDatabase, this.dateData);
+    this.update(Events.UpdateDatabase, this.dateData);
   }
 
   #renderEmotions() {
@@ -97,9 +70,7 @@ export class DayComponent extends BaseComponent {
 
       const deleteButton = document.createElement("button");
       deleteButton.textContent = "DELETE";
-      deleteButton.addEventListener("click", () =>
-        this.#removeEmotionEntry(emotion)
-      );
+      deleteButton.addEventListener("click", () => this.#removeEmotionEntry(emotion));
 
       emotionEntry.appendChild(deleteButton);
     });
@@ -132,35 +103,15 @@ export class DayComponent extends BaseComponent {
   }
 
   _addEventListeners() {
-    const hub = EventHub.getInstance();
-    hub.subscribe(Events.LoadDayPage, (data) => this.loadPage(data));
-    hub.subscribe(Events.SummarySubmitted, (journal) =>
-      this.#addJournalEntry(journal)
-    );
-    hub.subscribe(Events.CheckInSubmitted, (emotion) =>
-      this.#addEmotionEntry(emotion)
-    );
-
-    document
-      .getElementById("dayToMain")
-      .addEventListener("click", () => this.#goToMainPage());
-    document
-      .getElementById("dayToJournal")
-      .addEventListener("click", () => this.#goToJournalPage());
-    document
-      .getElementById("dayToCheckIn")
-      .addEventListener("click", () => this.#goToCheckInPage());
+    this.addEvent(Events.LoadDayPage, (data) => this.loadPage(data));
   }
 
   // Changes view to Day Page
   _render(data) {
     if (data) this.dateData = data;
 
-    document.getElementById("dayDate").textContent = dateFormat(
-      this.dateData.date_id
-    );
-    document.getElementById("dayJournalEntry").textContent =
-      this.dateData.journal;
+    this.titleDate.textContent = dateFormat(this.dateData.date_id);
+    this.journalEntry.textContent = this.dateData.journal;
 
     // Added Emotions to Log
     this.#renderEmotions();
