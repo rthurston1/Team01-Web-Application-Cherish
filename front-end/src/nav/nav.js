dd35cb1479d9657a3a24f8dbda92f3f373948482
rthurston1 <rthurston@umass.edu>
2024-11-13 01:30:08 -0500

    Navigation waits until LoadNav event is called to initialize

--- front-end/src/nav/nav.js
+++ front-end/src/nav/nav.js
@@ -1,48 +1,50 @@
+import { BaseComponent } from "../BaseComponent.js";
 import { EventHub } from "../eventhub/EventHub.js";
 import { Events } from "../eventhub/Events.js";
 
-/**
- * Navigates to the specified page.
- * @param {*} page The page to navigate to. Options are "check-in", "journal",
- * "stats", "summary", and "calendar".
- */
+const hub = EventHub.getInstance();
 
-function goToPage(page) {
-  const hub = EventHub.getInstance();
-  switch (page) {
-    case "check-in":
-      hub.publish(Events.LoadCheckInPage, date);
-      break;
-    case "journal":
-      hub.publish(Events.LoadJournalPage, date);
-      break;
-    case "stats":
-      hub.publish(Events.LoadStatsPage, date);
-      break;
-    case "summary":
-      hub.publish(Events.LoadSummaryPage, date);
-      break;
-    case "calendar":
-      hub.publish(Events.LoadMainPage, date);
-      break;
-    default:
-      hub.publish(Events.LoadMainPage, date);
-      break;
+hub.subscribe(Events.LoadNav, (data) => {
+    /**
+   * Navigates to the specified page.
+   * @param {*} page The page to navigate to. Options are "check-in", "journal",
+   * "stats", "summary", and "calendar".
+   */
+
+  function goToPage(page) {
+    switch (page) {
+      case "check-in":
+        hub.publish(Events.LoadCheckInPage, data);
+        break;
+      case "journal":
+        hub.publish(Events.LoadJournalPage, data);
+        break;
+      case "stats":
+        hub.publish(Events.LoadStatsPage, data);
+        break;
+      case "summary":
+        hub.publish(Events.LoadSummaryPage, data);
+        break;
+      case "calendar":
+        hub.publish(Events.LoadMainPage, data);
+        break;
+      default:
+        hub.publish(Events.LoadMainPage, data);
+        break;
+    }
   }
-}
 
-function buildHTML() {
-  return `
-            <button class="feature-button calendar-page" id="main_toCalendarPage">Calendar</button>
-            <button class="feature-button check-in" id="main_toCheckInPage">Check-in</button>
-            <button class="feature-button journal" id="main_toJournalPage">Journal</button>
-            <button class="feature-button stats" id="main_toStatsPage">Stats</button>
-            <button class="feature-button summary" id="main_toSummaryPage">Summary</button>
-    `;
-}
+  function buildHTML() {
+    return `
+              <button class="feature-button calendar-page" id="main_toCalendarPage">Calendar</button>
+              <button class="feature-button check-in" id="main_toCheckInPage">Check-in</button>
+              <button class="feature-button journal" id="main_toJournalPage">Journal</button>
+              <button class="feature-button stats" id="main_toStatsPage">Stats</button>
+              <button class="feature-button summary" id="main_toSummaryPage">Summary</button>
+      `;
+  }
 
-function addEventListeners() {
-  document.addEventListener("DOMContentLoaded", () => {
+  function addEventListeners() {
     document
       .getElementById("main_toCalendarPage")
       .addEventListener("click", () => goToPage("calendar"));
@@ -58,12 +60,13 @@ function addEventListeners() {
     document
       .getElementById("main_toSummaryPage")
       .addEventListener("click", () => goToPage("summary"));
-  });
-}
+  }
 
-function render() {
-  document.getElementsByClassName("nav")[0].innerHTML = buildHTML();
-  addEventListeners();
-}
+  function render() {
+    document.getElementsByClassName("nav")[0].innerHTML = buildHTML();
+    addEventListeners();
+  }
 
-render();
+  render();
+});
+
