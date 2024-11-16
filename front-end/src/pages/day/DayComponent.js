766eda5ccfd786e66eb1ee473f6b0f44e79898bb
rthurston1 <rthurston@umass.edu>
2024-11-15 22:58:09 -0500

    Fixed bugs on day page (edge case where deleting all emotions wouldn't show "NO EMOTIONS LOGGED" after deletion)

--- front-end/src/pages/day/DayComponent.js
+++ front-end/src/pages/day/DayComponent.js
@@ -37,52 +37,51 @@ export class DayComponent extends BaseComponent {
     // To get the check-in numeric rating, we will multiply baseline emoji value by magnitude of emotion value 
     // e.g. highest possible rating is happy with 10 magnitude (2 * 10 = 20), lowest possible rating is Disgusted with 10 magnitude (-4 * 10 = -40)
     // Then we normalize the final rating to fit within the 1 to 10 range
+    if (!this.dateData["emotions"] || this.dateData.emotions.length === 0) {
+      this.dateData["rating"] = 0;
+      return;
+    }
     
     let sumRate = 0, count = 0, dailyRating = 0; 
+    const emotionsArr = this.dateData.emotions;
+    emotionsArr.forEach((emotionObj) => {
+      let base = 0; 
+      const magnitude = emotionObj.magnitude;
+      switch (emotionObj.emotion_id) {
+        case "Happy":
+          base = 2;
+          break;
+        case "Neutral":
+          base = 0; 
+          break; 
+        case "Anxious":
+          base = -1;
+          break;
+        case "Sad":
+          base = -2;
+          break; 
+        case "Angry":
+          base = -3; 
+          break; 
+        case "Disgusted":
+          base = -4; 
+          break; 
+        default:
+          base = 0; 
+          break; 
+      }
 
-    if (!this.dateData.emotions || this.dateData.emotions.length === 0) {
-      dailyRating = 5; //If emotions attribute doesn't exist or no emotions stored yet, set to 5 (neutral)
-    } else {
-      const emotionsArr = this.dateData.emotions;
+      const rate = base * magnitude; //the rating for a single check-in 
+      sumRate += rate; //add each check-in rating to an accumulating totalRating value
+      count++;  
+    });
 
-      emotionsArr.forEach((emotionObj) => {
-        let base = 0; 
-        const magnitude = emotionObj.magnitude;
-        switch (emotionObj.emotion_id) {
-          case "Happy":
-            base = 2;
-            break;
-          case "Neutral":
-            base = 0; 
-            break; 
-          case "Anxious":
-            base = -1;
-            break;
-          case "Sad":
-            base = -2;
-            break; 
-          case "Angry":
-            base = -3; 
-            break; 
-          case "Disgusted":
-            base = -4; 
-            break; 
-          default:
-            base = 0; 
-            break; 
-        }
-
-        const rate = base * magnitude; //the rating for a single check-in 
-        sumRate += rate; //add each check-in rating to an accumulating totalRating value
-        count++;  
-      });
-
-      //normalize the final rating to fit within the 1 to 10 range
-      const maxPossibleRating = 2 * 10; //highest possible rating (Happy with magnitude 10)
-      const minPossibleRating = -4 * 10; //lowest possible rating (Disgusted with magnitude 10)
-      const normalizedRating = ((sumRate / count - minPossibleRating) / (maxPossibleRating - minPossibleRating)) * 9 + 1;
-      dailyRating = parseFloat(normalizedRating.toFixed(2)); //rounds rating to two decimal points
-    }
+    //normalize the final rating to fit within the 1 to 10 range
+    const maxPossibleRating = 2 * 10; //highest possible rating (Happy with magnitude 10)
+    const minPossibleRating = -4 * 10; //lowest possible rating (Disgusted with magnitude 10)
+    const normalizedRating = ((sumRate / count - minPossibleRating) / (maxPossibleRating - minPossibleRating)) * 9 + 1;
+    dailyRating = parseFloat(normalizedRating.toFixed(2)); //rounds rating to two decimal points
+    
 
     this.dateData["rating"] = dailyRating; // Store the daily rating in dateData object
     this.update(Events.StoreData, this.dateData);
@@ -90,7 +89,7 @@ export class DayComponent extends BaseComponent {
 
   #renderEmotions() {
     this.emotionLog.innerHTML = "" // Clears html 
-    if (!this.dateData.emotions) {
+    if (!this.dateData["emotions"] || this.dateData.emotions.length === 0) {
       this.emotionLog.textContent = "NO EMOTIONS LOGGED";
       return;
     }
