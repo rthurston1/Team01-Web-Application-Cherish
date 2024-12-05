42a62e4ca6bd6a7cec227fa0607c3c7051b9204a
rthurston1 <rthurston@umass.edu>
2024-12-05 10:58:54 -0500

    model factory #114 and #104. Implemented model factory class to allow an instance to our database server

--- back-end/src/model/ModelFactory.js
+++ back-end/src/model/ModelFactory.js
@@ -2,7 +2,19 @@
 /* We can use the local model at first and swap to the SQLite model later */
 
 class _ModelFactory {
-  /* TODO */
+  async getModel(model = "sqlite") {
+    switch (model) {
+      case "local":
+        return LocalDayModel;
+      case "sqlite":
+        return SQLiteDayModel;
+      case "sqlite-fresh":
+        await SQLiteDayModel.init(true);
+        return SQLiteDayModel;
+      default:
+        throw new Error("Model not found");
+    }
+  }
 }
 
 const ModelFactory = new _ModelFactory();
