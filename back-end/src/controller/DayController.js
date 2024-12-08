5116a543d114927380609300a8037fdad85618a4
rthurston1 <rthurston@umass.edu>
2024-12-08 01:08:37 -0500

    Added comments for a clearer understand of how the functions should be implemented

--- back-end/src/controller/DayController.js
+++ back-end/src/controller/DayController.js
@@ -25,11 +25,11 @@ class DayController {
    *  Retrieves all data from the model and sends it as a JSON response.
    *  Logs the process and handles any errors that occur.
    *
-   *  @param {Object} req - The request object.
-   *  @param {Object} res - The response object.
+   *  @param {Object} request - The request object.
+   *  @param {Object} response - The response object.
    *  @returns {Promise<void>} - A promise that resolves when the data is sent or an error is handled.
    */
-  async getAllData(req, res) {
+  async getAllData(request, response) {
     debugLog(`DayController.getAllData`);
     try {
       const data = await this.model.read();
@@ -44,62 +44,97 @@ class DayController {
     }
   }
 
-  async getAllUsers(req, res) {
+  // Gets all users in the database (does not include their day data)
+  async getAllUsers(request, response) {
     debugLog(`DayController.getAllUsers`);
     // TODO: Implement this method
   }
 
-  async getUserByUsername(req, res) {
+  // Gets a user from the database
+  // Request param contains the username
+  async getUserByUsername(request, response) {
     debugLog(`DayController.getUserByUsername`);
     // TODO: Implement this method
   }
 
-  async registerUser(req, res) {
+  /**
+   * Adds a new user into the database and returns a JWT session token
+   * Request body contains the username and password (password needs to be encrypted)
+   */
+  async registerUser(request, response) {
     debugLog(`DayController.registerUser`);
     // TODO: Implement this method
   }
 
-  async loginUser(req, res) {
+  /**
+   * Attempts to login in the user and returns all of their data
+   * Request body contains a username and password inputted by the user
+   */
+  async loginUser(request, response) {
     debugLog(`DayController.loginUser`);
     // TODO: Implement this method
   }
 
-  async postUserData(req, res) {
+  /**
+   * Posts the user's data
+   * Request params contains the username
+   */
+  async postUserData(request, response) {
     debugLog(`DayController.postUserData`);
     // TODO: Implement this method
   }
 
-  async getUserData(req, res) {
+  /**
+   * Gets all the user's data
+   * Request params contains the username
+   */
+  async getUserData(request, response) {
     debugLog(`DayController.getUserData`);
     // TODO: Implement this method
   }
 
-  async postDay(req, res) {
+  /**
+   * Saves a day into the database
+   * Request params contains the username and date_id
+   */
+  async postDay(request, response) {
     debugLog(`DayController.postDay`);
     // TODO: Implement this method
   }
 
-  async getDay(req, res) {
+  /**
+   * Gets a day from the database
+   * Request params contains the username and date_id
+   */
+  async getDay(request, response) {
     debugLog(`DayController.getDay`);
     // TODO: Implement this method
   }
 
-  async getDaysOfMonth(req, res) {
+  /**
+   * Gets all the days in a month
+   * Request params contains the username, a month, and a year
+   */
+  async getDaysOfMonth(request, response) {
     debugLog(`DayController.getDaysOfMonth`);
     // TODO: Implement this method
   }
 
-  async getDaysOfYear(req, res) {
+  /**
+   * Gets all the days in a year
+   * Request params contains the username and a year
+   */
+  async getDaysOfYear(request, response) {
     debugLog(`DayController.getDaysOfYear`);
     // TODO: Implement this method
   }
 
-  async addEmotion(req, res) {
+  async addEmotion(request, response) {
     debugLog(`DayController.addEmotion`);
     // TODO: Implement this method
   }
 
-  async deleteEmotion(req, res) {
+  async deleteEmotion(request, response) {
     debugLog(`DayController.deleteEmotion`);
     // TODO: Implement this method
   }
