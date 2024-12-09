39a44310c39249b7000206496f3aca3a46cd40c0
NikoZBK <nikozbk@gmail.com>
2024-12-09 11:33:31 -0500

    Fix DayRoutes.js import for debugLog, tweaked debugLog to output the log is coming from back or front end

--- back-end/src/routes/DayRoutes.js
+++ back-end/src/routes/DayRoutes.js
@@ -1,5 +1,5 @@
 import express, { request, response } from "express";
-import { debugLog } from "../../config/debug";
+import { debugLog } from "../../config/debug.js";
 import DayController from "../controller/DayController.js";
 
 class DayRoutes {
@@ -46,26 +46,43 @@ class DayRoutes {
     });
 
     // Adds/Updates a day to the database
-    this.router.post("/v1/days/:username/:date_id", async (request, response) => {
-      debugLog(`POST /v1/days/${request.params.username}/${request.params.date_id}`);
-      DayController.postDay(request, response);
-    });
+    this.router.post(
+      "/v1/days/:username/:date_id",
+      async (request, response) => {
+        debugLog(
+          `POST /v1/days/${request.params.username}/${request.params.date_id}`
+        );
+        DayController.postDay(request, response);
+      }
+    );
 
     // Gets a specified day based on it's id
-    this.router.get("/v1/days/:username/:date_id(\\d{2}-\\d{2}-\\d{4})", async (request, response) => {
-      debugLog(`GET /v1/days/${request.params.username}/${request.params.date_id}`);
-      DayController.getDay(request, response);
-    });
+    this.router.get(
+      "/v1/days/:username/:date_id(\\d{2}-\\d{2}-\\d{4})",
+      async (request, response) => {
+        debugLog(
+          `GET /v1/days/${request.params.username}/${request.params.date_id}`
+        );
+        DayController.getDay(request, response);
+      }
+    );
 
     // Gets all the days in the specified month in a year
-    this.router.get("/v1/days/:username/:month/:year", async (request, response) => {
-      debugLog(`GET /v1/days/${request.params.username}/${request.params.month}/${request.params.year}`);
-      DayController.getDaysOfMonth(request, response);
-    });
+    this.router.get(
+      "/v1/days/:username/:month/:year",
+      async (request, response) => {
+        debugLog(
+          `GET /v1/days/${request.params.username}/${request.params.month}/${request.params.year}`
+        );
+        DayController.getDaysOfMonth(request, response);
+      }
+    );
 
     // Gets all the days in the specified year
     this.router.get("/v1/days/:username/:year", async (request, response) => {
-      debugLog(`GET /v1/days/${request.params.username}/${request.params.year}`);
+      debugLog(
+        `GET /v1/days/${request.params.username}/${request.params.year}`
+      );
       DayController.getDaysOfYear(request, response);
     });
 
--- front-end/src/config/debug.js
+++ front-end/src/config/debug.js
@@ -2,6 +2,6 @@ import config from "./config.js";
 // do not use with sensitive data
 export function debugLog(message, label = "DEBUG") {
   if (config.debug) {
-    console.info(`[${label}]\t${message}`);
+    console.info(`[FRONT][${label}]\t${message}`);
   }
 }
